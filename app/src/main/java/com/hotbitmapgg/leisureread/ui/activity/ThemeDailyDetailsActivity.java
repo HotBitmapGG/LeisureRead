130e448d8933d8fe872f86f0f375d46be76984e2
hotshotGG <100332338@qq.com>
2016-12-30 16:00:03 +0800

    Update

--- app/src/main/java/com/hotbitmapgg/leisureread/ui/activity/ThemeDailyDetailsActivity.java
+++ app/src/main/java/com/hotbitmapgg/leisureread/ui/activity/ThemeDailyDetailsActivity.java
@@ -8,20 +8,18 @@ import com.hotbitmapgg.leisureread.network.RetrofitHelper;
 import com.hotbitmapgg.leisureread.ui.activity.base.BaseAppCompatActivity;
 import com.hotbitmapgg.leisureread.ui.adapter.ThemeDetailsHeadAdapter;
 import com.hotbitmapgg.leisureread.ui.adapter.ThemeDetailsStoriesAdapter;
-import com.hotbitmapgg.leisureread.utils.LogUtil;
 import com.hotbitmapgg.leisureread.widget.CircleProgressView;
 import com.hotbitmapgg.leisureread.widget.recycler.helper.HeaderViewRecyclerAdapter;
 import com.hotbitmapgg.rxzhihu.R;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -48,18 +46,19 @@ public class ThemeDailyDetailsActivity extends BaseAppCompatActivity {
   @Bind(R.id.recycle)
   RecyclerView mRecyclerView;
 
-  @Bind(R.id.swipe_refresh)
-  SwipeRefreshLayout mSwipeRefreshLayout;
+  private int id;
+
+  private ThemeDetailsInfo mThemeDetailsInfo;
+
+  private ThemeDetailsStoriesAdapter mAdapter;
+
+  private HeaderViewRecyclerAdapter mHeaderViewRecyclerAdapter;
 
   //主题日报故事列表
   private List<ThemeDetailsInfo.StoriesBean> stories = new ArrayList<>();
 
   //主题日报主编列表
   private List<ThemeDetailsInfo.EditorsBean> editors = new ArrayList<>();
-
-  private int id;
-
-  private HeaderViewRecyclerAdapter mHeaderViewRecyclerAdapter;
 
 
   @Override
@@ -76,100 +75,83 @@ public class ThemeDailyDetailsActivity extends BaseAppCompatActivity {
       id = intent.getIntExtra(AppConstant.EXTRA_TYPE, -1);
     }
 
-    startGetThemesDetails();
+    initRecyclerView();
+    loadData();
   }
 
 
-  private void startGetThemesDetails() {
-
-    mSwipeRefreshLayout.setColorSchemeResources(R.color.colorPrimaryDark);
-    mSwipeRefreshLayout.setOnRefreshListener(() -> mSwipeRefreshLayout.setRefreshing(false));
-    mCircleProgressView.setVisibility(View.VISIBLE);
-    mCircleProgressView.spin();
-    mRecyclerView.setVisibility(View.GONE);
-    getThemesDetails();
-  }
-
-
-  private void getThemesDetails() {
-
-    RetrofitHelper.builder().getThemesDetailsById(id)
-        .compose(bindToLifecycle())
-        .subscribeOn(Schedulers.io())
-        .observeOn(AndroidSchedulers.mainThread())
-        .subscribe(themesDetails -> {
-
-          if (themesDetails != null) {
-            finishGetThemesDetails(themesDetails);
-          }
-        }, throwable -> {
-
-          LogUtil.all("加载数据失败");
-        });
-  }
-
-
-  private void finishGetThemesDetails(ThemeDetailsInfo themeDetailsInfo) {
-
-    stories.addAll(themeDetailsInfo.getStories());
-    editors.addAll(themeDetailsInfo.getEditors());
-
-    mToolbar.setTitle(themeDetailsInfo.getName());
-
+  private void initRecyclerView() {
     mRecyclerView.setHasFixedSize(true);
-    LinearLayoutManager mLinearLayoutManager = new LinearLayoutManager(
-        ThemeDailyDetailsActivity.this);
-    mRecyclerView.setLayoutManager(mLinearLayoutManager);
-    ThemeDetailsStoriesAdapter mAdapter = new ThemeDetailsStoriesAdapter(mRecyclerView, stories);
+    mRecyclerView.setLayoutManager(new LinearLayoutManager(ThemeDailyDetailsActivity.this));
+    mAdapter = new ThemeDetailsStoriesAdapter(mRecyclerView, stories);
     mHeaderViewRecyclerAdapter = new HeaderViewRecyclerAdapter(mAdapter);
-    addHeadView(themeDetailsInfo);
     mRecyclerView.setAdapter(mHeaderViewRecyclerAdapter);
     mAdapter.setOnItemClickListener((position, holder) -> {
 
       ThemeDetailsInfo.StoriesBean stories1 = ThemeDailyDetailsActivity.this.stories.get(position);
       DailyDetailsActivity.lanuch(ThemeDailyDetailsActivity.this, stories1.getId());
     });
-
-    new Handler().postDelayed(() -> {
-
-      mCircleProgressView.setVisibility(View.GONE);
-      mCircleProgressView.stopSpinning();
-      mRecyclerView.setVisibility(View.VISIBLE);
-    }, 3000);
   }
 
 
-  private void addHeadView(ThemeDetailsInfo themeDetailsInfo) {
-
+  private void createHeadView() {
     View headView = LayoutInflater.from(ThemeDailyDetailsActivity.this)
         .inflate(R.layout.layout_themes_details_head, mRecyclerView, false);
+
     ImageView mThemesBg = (ImageView) headView.findViewById(R.id.type_image);
     TextView mThemesTitle = (TextView) headView.findViewById(R.id.type_title);
+    //设置主题日报背景图片
     Glide.with(ThemeDailyDetailsActivity.this)
-        .load(themeDetailsInfo.getBackground())
+        .load(mThemeDetailsInfo.getBackground())
         .placeholder(R.drawable.account_avatar)
         .into(mThemesBg);
-    mThemesTitle.setText(themeDetailsInfo.getDescription());
-    View editorsHeadView = LayoutInflater.from(ThemeDailyDetailsActivity.this)
-        .inflate(R.layout.layout_themes_details_head_2, mRecyclerView, false);
-    RecyclerView mHeadRecycle = (RecyclerView) editorsHeadView.findViewById(R.id.head_recycle);
-    mHeadRecycle.setHasFixedSize(true);
-    LinearLayoutManager mLinearLayoutManager = new LinearLayoutManager(
-        ThemeDailyDetailsActivity.this, LinearLayoutManager.HORIZONTAL, false);
-    mHeadRecycle.setLayoutManager(mLinearLayoutManager);
-    ThemeDetailsHeadAdapter mHeadAdapter = new ThemeDetailsHeadAdapter(mHeadRecycle, editors);
-    mHeadRecycle.setAdapter(mHeadAdapter);
-    mHeadAdapter.setOnItemClickListener((position, holder) -> {
+    //设置主题日报标题
+    mThemesTitle.setText(mThemeDetailsInfo.getDescription());
+    //设置主题日报编辑头像列表
+    RecyclerView mEditorsRecyclerView = (RecyclerView) headView.findViewById(R.id.head_recycle);
+    mEditorsRecyclerView.setHasFixedSize(true);
+    mEditorsRecyclerView.setLayoutManager(
+        new LinearLayoutManager(ThemeDailyDetailsActivity.this, LinearLayoutManager.HORIZONTAL,
+            false));
+    ThemeDetailsHeadAdapter mEditorsAdapter = new ThemeDetailsHeadAdapter(mEditorsRecyclerView,
+        editors);
+    mEditorsRecyclerView.setAdapter(mEditorsAdapter);
+    mEditorsAdapter.setOnItemClickListener((position, holder) ->
+        EditorInfoActivity.launch(ThemeDailyDetailsActivity.this,
+            editors.get(position).getId(),
+            editors.get(position).getName()));
 
-      ThemeDetailsInfo.EditorsBean editor = ThemeDailyDetailsActivity.this.editors.get(position);
-      int id1 = editor.getId();
-      String name = editor.getName();
-      EditorInfoActivity.launch(ThemeDailyDetailsActivity.this, id1, name);
-    });
+    //刷新数据
     mHeaderViewRecyclerAdapter.addHeaderView(headView);
-    mHeaderViewRecyclerAdapter.addHeaderView(editorsHeadView);
+    mAdapter.notifyDataSetChanged();
+    mEditorsAdapter.notifyDataSetChanged();
     mHeaderViewRecyclerAdapter.notifyDataSetChanged();
-    mHeadAdapter.notifyDataSetChanged();
+  }
+
+
+  private void loadData() {
+    RetrofitHelper.builder().getThemesDetailsById(id)
+        .compose(bindToLifecycle())
+        .doOnSubscribe(this::showProgress)
+        .delay(3000, TimeUnit.MILLISECONDS)
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(themeDetailsInfo -> {
+          mThemeDetailsInfo = themeDetailsInfo;
+          finishTask();
+        }, throwable -> {
+          hideProgress();
+        });
+  }
+
+
+  private void finishTask() {
+
+    mToolbar.setTitle(mThemeDetailsInfo.getName());
+    stories.addAll(mThemeDetailsInfo.getStories());
+    editors.addAll(mThemeDetailsInfo.getEditors());
+    hideProgress();
+    createHeadView();
   }
 
 
@@ -178,6 +160,20 @@ public class ThemeDailyDetailsActivity extends BaseAppCompatActivity {
     mToolbar.setNavigationIcon(R.drawable.ic_action_back);
     mToolbar.setNavigationOnClickListener(view -> onBackPressed());
     mToolbar.setTitleTextColor(getResources().getColor(R.color.black_90));
+  }
+
+
+  public void showProgress() {
+    mCircleProgressView.setVisibility(View.VISIBLE);
+    mCircleProgressView.spin();
+    mRecyclerView.setVisibility(View.GONE);
+  }
+
+
+  public void hideProgress() {
+    mCircleProgressView.setVisibility(View.GONE);
+    mCircleProgressView.stopSpinning();
+    mRecyclerView.setVisibility(View.VISIBLE);
   }
 
 
